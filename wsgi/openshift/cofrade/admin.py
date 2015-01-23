363abbdacaa39fe18a709048fa50d4627156a960
Jesus Bueno <jesus@DEBIAN>
2015-01-23 12:24:42 +0100

    a

---
+++ wsgi/openshift/cofrade/admin.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from cofrade.models import Cofradia,Paso
+from django.contrib import admin
+
+admin.site.register(Cofradia)
+admin.site.register(Paso)
--- wsgi/openshift/cofrade/models.py
+++ wsgi/openshift/cofrade/models.py
@@ -1,28 +1,29 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
-from django.contrib import admin
 
 # Create your models here.
 
 class Cofradia(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=300)
     nombrecorto = models.CharField(max_length=100)
     sede = models.CharField(max_length=50)
     annofundacion = models.IntegerField()
-    diaprocesion = models.CharField(max_length=20)
     numhermanos = models.IntegerField()
     hermanomayor = models.CharField(max_length=25)
+    diaprocesion = models.CharField(max_length=20)
+    hora = models.DateTimeField()
+    def __unicode__(self):
+        return self.nombrecorto
 
 class Paso(models.Model):
-    codigocofradia = models.ForeignKey(Cofradia)
+    cofradia = models.ForeignKey(Cofradia)
     nombre = models.CharField(max_length=50)
-    tipo = models.CharField(max_length=10)
     numcostaleros = models.IntegerField()
     numnazarenos = models.IntegerField()
+    numpasos = models.IntegerField()
     colorcapa = models.CharField(max_length=10)
     colorantifaz =  models.CharField(max_length=10)
-
-admin.site.register(Cofradia)
-admin.site.register(Paso)
-
-
+    def __unicode__(self):
+        return "%s, %s" % (self.cofradia,self.nombre)
 
