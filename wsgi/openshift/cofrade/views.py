bc74c1d5af2e191751f9284a0e333edfee055f5d
Jesús Bueno <jesusba60@gmail.com>
2015-02-25 17:12:10 +0100

    Creando vistas

--- wsgi/openshift/cofrade/views.py
+++ wsgi/openshift/cofrade/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect, Http404, HttpResponse
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+from django.contrib import auth
+import requests,json
+from cofrade import models
+from django.contrib.auth.models import User
+from cofrade.functions import *
+
+def login(request):
+    return render(request, 'home.html', {})
--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'openshift.views.home', name='home'),
+    # url(r'^$', 'openshift.views.home', name='home'),
     # url(r'^openshift/', include('openshift.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -14,4 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'openshift.cofrade.views.home'),
 )
