927dae4403901e2646b42984ee4427dabc1b5f35
Jesús Bueno <jesusba60@gmail.com>
2015-02-25 17:45:37 +0100

    Eliminando imports no utilizados

--- wsgi/openshift/cofrade/views.py
+++ wsgi/openshift/cofrade/views.py
@@ -1,12 +1,9 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
+import os
 from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
-from django.contrib import auth
 import requests,json
 from cofrade import models
-from django.contrib.auth.models import User
 from cofrade.functions import *
 
 def home(request):
