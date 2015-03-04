afe1f59b5e31beb09519ee1dd257316428678512
Jesús Bueno <jesusba60@gmail.com>
2015-03-04 10:26:24 +0100

    a

--- wsgi/openshift/cofrade/views.py
+++ wsgi/openshift/cofrade/views.py
@@ -1,10 +1,8 @@
 import os
-from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 import requests,json
 from cofrade import models
-from cofrade.functions import *
 
 def home(request):
     return render_to_response('home.html')
