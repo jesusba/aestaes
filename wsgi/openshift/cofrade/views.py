2bd3cbb914a6f3fa86a3c87df80080de9a9b554f
Jesús Bueno <jesusba60@gmail.com>
2015-03-05 17:55:07 +0100

    a

--- wsgi/openshift/cofrade/views.py
+++ wsgi/openshift/cofrade/views.py
@@ -2,15 +2,16 @@ import os
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 import requests,json
-from cofrade import models
+from cofrade.models import Cofradia,Paso
+
 
 def home(request):
     return render_to_response('home.html')
 
 def nombrecofra(request):
-    cofradia = models.Cofradia.objects.all()
+    cofradia = Cofradia.objects.all()
     return render_to_response('cofradias.html',{'data_raw': cofradia})
 
 def nombrepaso(request):
-    paso = models.Paso.objects.all()
+    paso = Paso.objects.all()
     return render_to_response('pasos.html',{'data_raw': paso})
